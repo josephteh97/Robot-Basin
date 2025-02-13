e10748ead0cc8917edb90dda318899d792f54710
josephteh97 <1079183153@qq.com>
2025-02-13 16:21:05 +0800

    this is a good environment for isaacgym

---
+++ environments/cleaning_env9.py
@@ -0,0 +1,135 @@
+from isaacgym import gymapi
+import random
+
+# Initialize gym
+gym = gymapi.acquire_gym()
+
+# Select GPU for processing
+compute_device_id = 0
+graphics_device_id = 0
+
+sim_params = gymapi.SimParams()
+sim = gym.create_sim(compute_device_id, graphics_device_id, gymapi.SIM_PHYSX, sim_params)
+
+# get default set of parameters
+sim_params = gymapi.SimParams()
+
+# set common parameters
+sim_params.dt = 1 / 60
+sim_params.substeps = 2
+sim_params.up_axis = gymapi.UP_AXIS_Z
+sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+
+# set PhysX-specific parameters
+sim_params.physx.use_gpu = True
+sim_params.physx.solver_type = 1
+sim_params.physx.num_position_iterations = 6
+sim_params.physx.num_velocity_iterations = 1
+sim_params.physx.contact_offset = 0.01
+sim_params.physx.rest_offset = 0.0
+
+# set Flex-specific parameters
+sim_params.flex.solver_type = 5
+sim_params.flex.num_outer_iterations = 4
+sim_params.flex.num_inner_iterations = 20
+sim_params.flex.relaxation = 0.8
+sim_params.flex.warm_start = 0.5
+
+# set the physics engine to PhysX
+physics_engine = gymapi.SIM_PHYSX
+
+# Up Axis
+sim_params.up_axis = gymapi.UP_AXIS_Z
+sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+
+# create sim with these parameters
+# sim = gym.create_sim(compute_device_id, graphics_device_id, physics_engine, sim_params)
+
+# configure the ground plane
+plane_params = gymapi.PlaneParams()
+plane_params.normal = gymapi.Vec3(0, 0, 1) # z-up!
+plane_params.distance = 0
+plane_params.static_friction = 1
+plane_params.dynamic_friction = 1
+plane_params.restitution = 0
+
+# create the ground plane
+gym.add_ground(sim, plane_params)
+
+# Load Assets
+asset_root = "./environments"
+asset_file = "washbasin.xml"
+asset = gym.load_asset(sim, asset_root, asset_file)
+
+asset_options = gymapi.AssetOptions()
+asset_options.fix_base_link = True
+asset_options.armature = 0.01
+
+asset = gym.load_asset(sim, asset_root, asset_file, asset_options)
+print("Assets Loaded!")
+
+spacing = 2.0
+lower = gymapi.Vec3(-spacing, 0.0, -spacing)
+upper = gymapi.Vec3(spacing, spacing, spacing)
+
+env = gym.create_env(sim, lower, upper, 8)
+
+pose = gymapi.Transform()
+pose.p = gymapi.Vec3(0.0, 1.0, 0.0)
+pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
+
+actor_handle = gym.create_actor(env, asset, pose, "MyActor", 0, 1)
+
+pose.r = gymapi.Quat.from_axis_angle(gymapi.Vec3(1, 0, 0), -0.5 * 3.142)
+
+# set up the env grid
+num_envs = 1
+envs_per_row = 1
+env_spacing = 2.0
+env_lower = gymapi.Vec3(-env_spacing, 0.0, -env_spacing)
+env_upper = gymapi.Vec3(env_spacing, env_spacing, env_spacing)
+
+# cache some common handles for later use
+envs = []
+actor_handles = []
+
+# create and populate the environments
+for i in range(num_envs):
+    env = gym.create_env(sim, env_lower, env_upper, envs_per_row)
+    envs.append(env)
+
+    height = random.uniform(1.0, 2.5)
+
+    pose = gymapi.Transform()
+    pose.p = gymapi.Vec3(0.0, height, 0.0)
+
+    actor_handle = gym.create_actor(env, asset, pose, "MyActor", i, 1)
+    actor_handles.append(actor_handle)
+
+cam_props = gymapi.CameraProperties()
+
+# Create the viewer *before* setting the camera properties
+viewer = gym.create_viewer(sim, cam_props)
+
+
+
+while not gym.query_viewer_has_closed(viewer):
+
+    # step the physics
+    gym.simulate(sim)
+    gym.fetch_results(sim, True)
+
+    # update the viewer
+    gym.step_graphics(sim);
+    gym.draw_viewer(viewer, sim, True)
+
+    # Wait for dt to elapse in real time.
+    # This synchronizes the physics simulation with the rendering rate.
+    gym.sync_frame_time(sim)
+
+
+
+
+# # Cleanup
+gym.destroy_viewer(viewer)
+gym.destroy_sim(sim)
